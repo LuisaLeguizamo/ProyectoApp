26828bbacb8d72aa4346b258aeb13d1f188150d5
luisaleguizamo <luisaleguizamo96@gmail.com>
2024-07-09 15:38:05 -0500

    Trabajo final

---
+++ src/controllers/controlador.js
@@ -0,0 +1,10 @@
+function cambiarCaja(){
+    let boton=document.getElementById("botonChat")
+    let cajaChat=document.getElementById("cajaChat")
+
+    if(cajaChat.style.display=='none' || cajaChat.style.display==''){
+        cajaChat.style.display="block"
+    }else{
+        cajaChat.style.display="none"
+    }
+}
