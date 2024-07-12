c0bc9cecb1cfb861503911724c08ab6555339190
luisaleguizamo <luisaleguizamo96@gmail.com>
2024-07-12 15:57:42 -0500

    nueva actualizacion

--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
+import { getAuth, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js'
+
 function cambiarCaja(){
     let boton=document.getElementById("botonChat")
     let cajaChat=document.getElementById("cajaChat")
@@ -8,3 +12,33 @@ function cambiarCaja(){
         cajaChat.style.display="none"
     }
 }
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBvv5eTEt0rnX6WyFgPuOzLfBljtTboCoE",
+    authDomain: "loginsuradualll.firebaseapp.com",
+    projectId: "loginsuradualll",
+    storageBucket: "loginsuradualll.appspot.com",
+    messagingSenderId: "942432322796",
+    appId: "1:942432322796:web:281e40ee553c0901f1d1c9",
+    measurementId: "G-6WCZ17HPFL"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  let avatar=document.getElementById("avatar")
+  let botonsalir=document.getElementById("botonsalir")
+  //Utilizando la información del usuario loggeado
+  const auth=getAuth()
+  onAuthStateChanged(auth,(user)=>{
+    if(user){
+        let uid=user.uid
+        avatar.classList.remove("invisible")
+        avatar.textContent=user.email
+    }else{
+
+    }
+  })
+
+  botonsalir.addEventListener("click", function(){
+    alert("Estás saliendo")
+  })
